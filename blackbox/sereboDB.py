6e22966a5596973d263181a3fe64575e141242e7
Maurice HT Ling <mauriceling@acm.org>
2018-06-18 01:33:47 +0800

    + added all needed data tables

--- blackbox/sereboDB.py
+++ blackbox/sereboDB.py
@@ -82,10 +82,48 @@ class SereboDB(object):
         insert into metadata (key, value) values ('blackboxID', '%s');
         ''' % (self.randomString(512))
         # Data log table
+        sql_datalog_create = '''
+        create table if not exists datalog (
+            ID integer primary key autoincrement,
+            dtstamp text not null,
+            hash text not null,
+            data blob);'''
+        sql_datalog_unique = '''
+        create unique index if not exists datalog_unique on datalog (
+            dtstamp, hash);'''
         # Blockchain table
+        sql_blockchain_create = '''
+        create table if not exists blockchain (
+            c_ID integer primary key autoincrement,
+            c_dtstamp text not null,
+            c_randomstring text not null,
+            c_hash text not null,
+            p_ID integer not null,
+            p_dtstamp text not null,
+            p_randomstring text not null,
+            p_hash text not null,
+            data text not null);'''
+        # Event log table
+        sql_eventlog_create1 = '''
+        create table if not exists eventlog (
+            ID integer primary key autoincrement,
+            dtstamp text not null,
+            fID text not null,
+            description text not null);'''
+        sql_eventlog_create2 = '''
+        create table if not exists eventlog_datamap (
+            fID text not null,
+            key text not null,
+            value text not null);'''
+        # SQL execution
         sqlstmt = [sql_metadata_create, 
                    sql_metadata_insert1,
-                   sql_metadata_insert2]
+                   sql_metadata_insert2,
+                   sql_datalog_create,
+                   sql_datalog_unique,
+                   sql_blockchain_create,
+                   sql_eventlog_create1,
+                   sql_eventlog_create2]
         for statement in sqlstmt:
             try:
                 self.cur.execute(statement)
